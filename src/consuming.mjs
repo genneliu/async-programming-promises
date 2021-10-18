5a36d3b3e96ac58209c255d80d99705f5e95aa74
Genne Liu <genneliu@gmail.com>
2021-10-18 10:27:18 -0700

    get and catch

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,12 +1,22 @@
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
 export function get() {
+    axios.get("httpy://localhost:3000/orders/1")
+    .then(({data}) => {
+        setText(JSON.stringify(data));
+    });
 }
 
 export function getCatch() {
+    axios.get("httpy://localhost:3000/orders/1")
+    .then(({data}) => {
+        setText(JSON.stringify(data));
+    })
+    .catch(err => setText(err));
 }
 
 export function chain() {
+
 }
 
 export function chainCatch() {
