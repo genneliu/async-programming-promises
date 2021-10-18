4a54223ed029d9d9bb26c41b54a18f1d21419a16
Genne Liu <genneliu@gmail.com>
2021-10-18 10:43:13 -0700

    chainCatch

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,3 +1,4 @@
+import { axios } from "./axios.min";
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
 export function get() {
@@ -16,10 +17,31 @@ export function getCatch() {
 }
 
 export function chain() {
-
+    axios.get("httpy://localhost:3000/orders/1")
+    .then(({data}) => {
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    })
+    .then(({data}) => {
+        setText(`City: ${data.city}`)
+    })
 }
 
+
+
 export function chainCatch() {
+    axios.get("httpy://localhost:3000/orders/1")
+    .then(({data}) => {
+         axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+         throw new Error("Error!");
+    })
+    .catch(err => {
+        setText(err);
+        throw new Error("Second Error")
+    })
+    .then(({data}) => {
+        setText(`City: ${data.my.city}`)
+    })
+    .catch(err => setText(err));
 }
 
 export function final() {
